5a7aab1fd16418ddd40cdcc479bf555bb4bd96dc
Chris Bunkers <christopher.bunkers@gmail.com>
2016-06-27 14:58:57 -0500

    add animated skills bar

--- public/js/application.js
+++ public/js/application.js
@@ -7,13 +7,13 @@
     var skill = $(this).attr("class");
     if(skill === "full-text"){
       if($("div.full-text").hasClass("hidden")){
-        $("div.full-text").removeClass("hidden").addClass("active");
+        $("div.full-text").removeClass("hidden").addClass("active", {duration:1000});
         $("div.front-text").addClass("hidden").removeClass("active");
       }
     }
     else{
       if($("div.front-text").hasClass("hidden")){
-        $("div.front-text").removeClass("hidden").addClass("active")
+        $("div.front-text").removeClass("hidden").addClass("active", {duration:1000});
         $("div.full-text").addClass("hidden").removeClass("active");
       }
     }
@@ -26,9 +26,38 @@
     $("#arrow").animate({opacity: 0.0},500);
    });
 
+  var offset = ($("#skills-bars").offset().top - ($("#skills-bars").height() + 150));
+  function testScroll(ev){
+    if($(".skillbar-bar").width() === 0){
+      if(window.pageYOffset >= offset){
+      // animate skill bar
+        $('.skillbar').each(function(){
+          $(this).find('.skillbar-bar').animate({
+            width:$(this).attr('data-percent')
+          },6000);
+        });
+      // animate percentage change
+        $('.skill-bar-percent').each(function () {
+          var display = $(this);
+          var nextValue = parseInt($(this).parent().attr('data-percent'));
+          var currentValue = 0;
+          var diff = nextValue - currentValue;
+          var time = 6000/diff;
+          for (var i = 0; i < Math.abs(diff); ++i) {
+            setTimeout(function() {
+                currentValue += 1;
+                display.text(currentValue+"%");
+            }, time * i);
+          }
+        });
+      }
+    }
+  }
+
+
+    window.onscroll=testScroll;
 
 
 
-
- });
+});
 
