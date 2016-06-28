e7a27d3ca50d429a78072bfec5ae410d006ad310
Chris Bunkers <christopher.bunkers@gmail.com>
2016-06-28 02:06:25 -0500

    now I'm really done

--- public/js/application.js
+++ public/js/application.js
@@ -20,7 +20,7 @@
   });
 
 
-   $("#mission").mouseenter(function(){
+   $("#mission > div >div").mouseenter(function(){
     $("#arrow").animate({opacity: 1.0},500);
    }).mouseleave(function(){
     $("#arrow").animate({opacity: 0.0},500);
